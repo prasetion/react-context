ce0406d3a19b2c4eb2782aab9ae29fabe160862b
prasetion <prasetion@gmail.com>
2025-03-14 16:43:36 +0700

    link context into state

--- src/App.jsx
+++ src/App.jsx
@@ -66,13 +66,18 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop />
     </CartContext.Provider>
   );
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,11 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const { items } = useContext(CartContext);
+
+  console.log("data before reduce", items);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
